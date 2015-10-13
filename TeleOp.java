26f9d88353979f68ffd311bd5872f50e48149d5e
Quest Jelinek <TofuTheBland@users.noreply.github.com>
2015-10-13 15:11:22 -0700

    Update TeleOp.java

--- TeleOp.java
+++ TeleOp.java
@@ -1,16 +1,19 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 /**
- * TODO Created by Quest Jelinek on 9/8/2015. 
+ * TODO Created by Quest Jelinek on 10/13/2015.
  * GitHub: TofuTheBland || https://github.com/TofuTheBland
  */
 public class TeleOp extends OpMode {
     // TODO Initiate all variables
-    DcMotor motor_left; //Forward Left
-    DcMotor motor_right;
+    DcMotor motor_leftFront; //Forward Left
+    DcMotor motor_rightFront;
+    DcMotor motor_leftBack; //Backward Left
+    DcMotor motor_rightBack;
     Float driver_gamepad_x;
     Float driver_gamepad_y;
 
@@ -21,47 +24,44 @@ public class TeleOp extends OpMode {
         motor_left = hardwareMap.dcMotor.get("motor_1");
         motor_right = hardwareMap.dcMotor.get("motor_2");
 
-
         //Added this for the lads
         driver_gamepad_x = gamepad1.left_stick_x;
         driver_gamepad_y = gamepad1.left_stick_y;
     }
 
+    public void forward() {
+        //For the method .setPower() pass a value from -1 to 1 (inc. 0)
+        motor_left.setPower(1);
+        motor_right.setPower(1);
+    }
+    public void backward() {
+        //For the method .setPower() pass a value from -1 to 1 (inc. 0)
+        motor_left.setPower(-1);
+        motor_right.setPower(-1);
+    }
+    public void left() {
+        //For the method .setPower() pass a value from -1 to 1 (inc. 0)
+        motor_left.setPower(1);
+        motor_right.setPower(-1);
+    }
+
+    public void right(){
+        motor_left.setPower(-1);
+        motor_left.setPower(1);
+    }
+
     @Override
     public void loop() {
 
-        // TODO Example method/action of the motor, in this case applying power
-
-
-        if(driver_gamepad_y > 100)
-        {
-            //For the method .setPower() pass a value from -1 to 1 (inc. 0)
-            motor_left.setPower(1);
-            motor_right.setPower(1);
-
-
-        }
-        else if (driver_gamepad_y < -100)
-        {
-            motor_left.setPower(-1);
-            motor_right.setPower(-1);
-        }
-
-        else if (driver_gamepad_x > 100)
-        {
-            motor_left.setPower(1);
-            motor_right.setPower(-1);
-        }
-        else if (driver_gamepad_x < -100)
-        {
-            motor_left.setPower(-1);
-            motor_right.setPower(1);
-        }
-        else
-        {
+        if(driver_gamepad_y > 100) {forward();}
+        else if (driver_gamepad_y < -100) {backward();}
+        else if (driver_gamepad_x > 100) {left();}
+        else if (driver_gamepad_x < -100) {right();}
+        else {
             motor_left.setPower(0);
             motor_right.setPower(0);
         }
-
     }
 }
+
+//TODO Add a separate library for movement functions and add smoothing function
